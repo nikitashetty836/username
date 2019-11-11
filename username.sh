d00412a391d21c52033b7b47b16eba5e27d6827a
Nikita Shetty <nshetty@chapman.edu>
2019-11-10 21:23:11 -0800

    Completed username.sh

--- username.sh
+++ username.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 # username.sh
 # Nikita Shetty
+
+echo "Enter a username."
 echo "The only characters that can be used are: "
 echo "   -lower case letters"
 echo "   -digits"
@@ -8,11 +10,11 @@ echo "   -the underscore character"
 echo "It must start with a lower case letter"
 echo "It must contain at least three but no more than 12 characters"
 
-read ZIP
-while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must enter a valid password!"
+	echo "Enter a password: "
+	read USERNAME
 done
 echo "Thank you"
